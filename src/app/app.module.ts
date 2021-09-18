54715606af3e51cbd7d9b7b435012d1f396b9750
Cristian Franco <cfrancobedoya@gmail.com>
2021-09-18 18:37:57 -0500

    Adding new assets

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { UserComponent } from './core/models/user/user.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UserComponent
   ],
   imports: [
     BrowserModule,
